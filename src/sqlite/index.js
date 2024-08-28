23c4a3e8aa29c5d073f87fa11b9b016a191478ad
User <User@WRSD-LD309-018.dut.ac.za>
2024-08-28 14:46:51 +0200

    feat-select

--- src/sqlite/index.js
+++ src/sqlite/index.js
@@ -8,3 +8,12 @@ const db = new sqlite3.Database('./music.db',sqlite3.OPEN_READWRITE, (err)=>{
 let sql=`CREATE TABLE users(id INTEGER PRIMARY KEY,first_name,last_name,password,email)`
 db.run(sql)
 
+//Select
+sql = 'SELECT * FROM users';
+db.all(sql,[],(err,rows)=>{
+    if(err) return console.error(err.message);
+    rows.forEach((row)=>{
+        console.log(row);
+    })
+})
+
