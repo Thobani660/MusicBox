9b9e5b54d5553a03b54b2238023fa2266d75b998
User <User@WRSD-LD309-018.dut.ac.za>
2024-08-28 15:08:07 +0200

    feat-select

--- src/sqlite/index.js
+++ src/sqlite/index.js
@@ -1,3 +1,5 @@
+let requirejs = require('requirejs');
+
 const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('./music.db',sqlite3.OPEN_READWRITE, (err)=>{
@@ -9,11 +11,11 @@ let sql=`CREATE TABLE users(id INTEGER PRIMARY KEY,first_name,last_name,password
 db.run(sql)
 
 //Select
-sql = 'SELECT * FROM users';
-db.all(sql,[],(err,rows)=>{
-    if(err) return console.error(err.message);
-    rows.forEach((row)=>{
-        console.log(row);
-    })
-})
+// sql = 'SELECT * FROM users';
+// db.all(sql,[],(err,rows)=>{
+//     if(err) return console.error(err.message);
+//     rows.forEach((row)=>{
+//         console.log(row);
+//     })
+// })
 
